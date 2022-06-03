a6cf4dadc2cf25065607691139a79d1abfbdb13e
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-03 13:40:06 -0300

    go-rest: config database connection via gorm

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go-rest/database"
 	"go-rest/models"
 	"go-rest/routes"
 )
@@ -12,6 +13,8 @@ func main() {
 		{Id: 1, Nome: "nome 1", Historia: "historia 1"},
 		{Id: 2, Nome: "nome 2", Historia: "historia 2"},
 	}
+
+	database.ConnectionDatabase()
 
 	fmt.Println("iniciando servidor")
 	routes.HandleRequest()
